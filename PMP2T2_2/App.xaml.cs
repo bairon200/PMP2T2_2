66e52c3eb67f0fa80c47b9fd5dccd849ad7f5f79
bairon200 <91520883+bairon200@users.noreply.github.com>
2022-07-12 12:15:55 -0600

    Agregar archivos de proyecto.

---
+++ PMP2T2_2/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using PMP2T2_2.Models;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PMP2T2_2
+{
+    public partial class App : Application
+    {
+
+        static basededatos basedatos;
+        public static basededatos BaseDatos
+        {
+            get
+            {
+                if (basedatos == null)
+                {
+                    basedatos = new basededatos(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "EmpleDB.db3"));
+                }
+                return basedatos;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PMP2T2_2/Models/constructor.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMP2T2_2.Models
+{
+    public class constructor
+    {
+        [PrimaryKey, AutoIncrement]
+        public int codigo { get; set; }
+
+        [MaxLength(250)]
+        public string nombre { get; set; }
+
+        [MaxLength(250)]
+        public string descripcion { get; set; }
+
+        public byte[] imgM { get; set; }
+    }
+}
